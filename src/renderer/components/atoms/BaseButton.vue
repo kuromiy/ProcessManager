e4b7f954f6e037ed101ca908be36b4aee3ad0a8d
Kuromiya <krmy3993@gmail.com>
2019-07-10 16:08:40 +0900

    BaseButton作成

---
+++ src/renderer/components/atoms/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button type="button" @click="click"><slot></slot></button>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit } from "vue-property-decorator";
+
+@Component({
+  name: "base-button"
+})
+export default class BaseButton extends Vue {
+  @Emit("click")
+  public click() {}
+}
+</script>
