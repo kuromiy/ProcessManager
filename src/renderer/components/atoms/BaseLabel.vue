c1ed001813958f6dfbd580891552fa41170de7df
Kuromiya <krmy3993@gmail.com>
2019-07-10 16:17:33 +0900

    BaseLabel 作成

---
+++ src/renderer/components/atoms/BaseLabel.vue
@@ -0,0 +1,12 @@
+<template>
+  <label><slot></slot></label>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "base-label"
+})
+export default class BaseLabel extends Vue {}
+</script>
