4c999ddd1141fc1ca46825f64e8bf4a2a8819a8f
Kuromiya <krmy3993@gmail.com>
2019-07-10 22:56:53 +0900

    プロセスの起動・停止実装

---
+++ scripts/script.ps1
@@ -0,0 +1,45 @@
+﻿Param($selfpid);
+
+<# 指定したidからプロセスを取得する #>
+function getProcessById($id) {
+  return Get-WmiObject -Class Win32_Process -Filter "ProcessId = $id"
+}
+
+function stopProcessById($id) {
+  Stop-Process -Id $id;
+}
+
+<#
+
+#>
+function checkProcess($process) {
+  if ($process.Handle -eq $process.ParentProcessId) {
+    return $false;
+  } elseif ($selfpid -eq $process.ParentProcessId) {
+    return $true;
+  } else {
+    $nextProcess = getProcessById($process.ParentProcessId);
+    checkProcess($nextProcess);
+  }
+}
+
+<# メイン関数 #>
+function main() {
+  Write-Host $selfpid;
+  $allprocess = Get-WmiObject Win32_Process;
+  $hitProcessIds = @();
+  foreach($process in $allprocess) {
+    $tmp = checkProcess($process);
+    if ($tmp) {
+      Write-Host -NoNewline $process.ExecutablePath $process.Handle $process.ParentProcessId;
+      $hitProcessIds += $process.Handle;
+    }
+  }
+
+  foreach($hitProcessId in $hitProcessIds) {
+    # 存在しないプロセスIDを指定するとエラーが起きる。
+    stopProcessById($hitProcessId);
+  }
+}
+
+main;
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow } from "electron";
+import { ChildProcess, spawn } from "child_process";
+import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
 
 let win: BrowserWindow | null;
@@ -31,4 +32,62 @@ app.on("window-all-closed", () => {
 
 app.on("activate", () => {
   if (win === null) createWindow();
+});
+
+const processMap: Map<number, ChildProcess> = new Map<number, ChildProcess>();
+ipcMain.on("startExec", (event: Event, arg: {exec: any, args: any, folder: any, processid: any}) => {
+  const tmp = arg.args.split(" ");
+  const proc = spawn(`${arg.exec}`, tmp, {cwd: arg.folder});
+  processMap.set(arg.processid, proc);
+
+  proc.stdout.on("data", (data) => {
+    console.log(`${data}`);
+    if (win) win.webContents.send("message", {context: data, process_id: arg.processid});
+  });
+
+  proc.stderr.on("data", (data) => {
+    console.log(`${data}`);
+    if (win) win.webContents.send("message", {context: data, process_id: arg.processid});
+  });
+
+  proc.on("error", (err: Error) => {
+    console.log("err");
+    console.log(err);
+    if (win) win.webContents.send("message", {context: "error", process_id: arg.processid});
+  });
+
+  proc.on("close", (code: number, signal: string) => {
+    console.log("close");
+    if (win) win.webContents.send("message", {context: "close", process_id: arg.processid});
+  });
+
+  proc.on("exit", (code: number, signal: string) => {
+    console.log("exit");
+    if (win) win.webContents.send("message", {context: "exit", process_id: arg.processid});
+  });
+});
+
+ipcMain.on("closeExec", (event: Event, arg: {processid: any}) => {
+  const proc: ChildProcess | undefined = processMap.get(arg.processid);
+  if (proc) {
+    const psPath = path.join(__dirname, "../scripts/script.ps1");
+    const pr = spawn("powershell.exe", [psPath, String(proc.pid)]);
+    pr.stdout.on("data", (data) => {
+      console.log(`${data}`);
+    });
+    pr.stderr.on("data", (data) => {
+      console.log(`${data}`);
+    });
+    pr.on("error", () => {
+      console.log("error");
+    });
+    pr.on("close", () => {
+      console.log("close");
+      if (win) win.webContents.send("message", {context: "close2", process_id: arg.processid});
+    });
+    pr.on("exit", () => {
+      console.log("exit");
+      if (win) win.webContents.send("message", {context: "exit2", process_id: arg.processid});
+    });
+  }
 });
