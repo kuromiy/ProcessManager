13d49ad2cfcdc2a121457cf95179af5763185b63
Kuromiya <krmy3993@gmail.com>
2019-07-10 16:20:21 +0900

    BaseSelect 作成

---
+++ src/renderer/components/atoms/BaseSelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <select :value="value" @input="updateSelect">
+    <slot v-bind:opts="opts"></slot>
+  </select>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+
+@Component({
+  name: "base-select"
+})
+export default class BaseSelect extends Vue {
+  @Prop(String) readonly value: string;
+  @Prop({ type: Array, required: true }) readonly opts: Array<string>;
+
+  @Emit("input")
+  public updateSelect(e: any) {
+    return e.target.value;
+  }
+}
+</script>
