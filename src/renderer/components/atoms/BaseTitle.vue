46196fff96366a854c395f7aef62f1d15d9b1839
Kuromiya <krmy3993@gmail.com>
2019-07-10 16:21:39 +0900

    BaseTitle 作成

---
+++ src/renderer/components/atoms/BaseTitle.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1><slot></slot></h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "base-title"
+})
+export default class BaseTitle extends Vue {}
+</script>
