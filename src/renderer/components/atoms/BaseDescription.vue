8d8a3c105365f7431bf54727b88b2361a317265c
Kuromiya <krmy3993@gmail.com>
2019-07-10 16:16:04 +0900

    BaseDescription 作成

---
+++ src/renderer/components/atoms/BaseDescription.vue
@@ -0,0 +1,12 @@
+<template>
+  <p><slot></slot></p>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "base-description"
+})
+export default class BaseDescription extends Vue {}
+</script>
