27d383df91d67636d2df33126e5c1160e1632888
Kuromiya <krmy3993@gmail.com>
2019-07-10 16:27:44 +0900

    BaseTable 作成

---
+++ src/renderer/components/atoms/BaseTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th
+          v-for="(header, index) in headers"
+          :key="index">
+          {{header}}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(data, index) in datas"
+        :key="index">
+        <slot v-bind:data="data"></slot>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "base-table"
+})
+export default class BaseTable extends Vue {
+  @Prop({ type: Array, required: true }) readonly headers: Array<string>;
+  @Prop({ type: Array, required: true }) readonly datas: Array<any>;
+}
+</script>
