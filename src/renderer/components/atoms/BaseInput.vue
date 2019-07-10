4cb9afcc31eba27a3ccd7800bdd981e2b96755da
Kuromiya <krmy3993@gmail.com>
2019-07-10 16:13:53 +0900

    BaseInput 作成

---
+++ src/renderer/components/atoms/BaseInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" :value="value" @input="updateText" />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+
+@Component({
+  name: "base-input"
+})
+export default class BaseInput extends Vue {
+  @Prop(String) readonly value: string;
+
+  @Emit("input")
+  public updateText(e: any) {
+    return e.target.value;
+  }
+}
+</script>
