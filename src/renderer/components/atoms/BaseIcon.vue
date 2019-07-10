4626945cfe110e0f3a1531e0d28d248e1e472d0d
Kuromiya <krmy3993@gmail.com>
2019-07-10 16:24:01 +0900

    BaseIcon 作成

---
+++ src/renderer/components/atoms/BaseIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div @click="click"><slot></slot></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit } from "vue-property-decorator";
+
+@Component({
+  name: "base-icon"
+})
+export default class BaseIcon extends Vue {
+  @Emit("click")
+  public click() {}
+}
+</script>
