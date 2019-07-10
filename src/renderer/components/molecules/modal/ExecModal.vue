652779ba7fe0f75d2a8714f3513b6959c59a5109
Kuromiya <krmy3993@gmail.com>
2019-07-10 18:56:05 +0900

    ExecModal 作成

---
+++ src/renderer/components/molecules/modal/ExecModal.vue
@@ -0,0 +1,86 @@
+<template>
+  <BaseModal @clickOutsideWindow="clickOutsideWindow">
+    <template v-slot:header>
+      <BaseTitle>新規実行</BaseTitle>
+    </template>
+    <template v-slot:content>
+      <div class="exec-modal-content">
+        <BaseLabel>実行名</BaseLabel>
+        <BaseInput v-model="execName"/>
+        <BaseLabel>ファイルパス</BaseLabel>
+        <BaseInput v-model="execPath"/>
+        <BaseButton @click="clickSelectFile">選択</BaseButton>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <BaseButton @click="clickCancel">キャンセル</BaseButton>
+      <BaseButton @click="clickRegist">追加</BaseButton>
+    </template>
+  </BaseModal>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit } from "vue-property-decorator";
+import BaseModal from "../../atoms/BaseModal.vue";
+import BaseTitle from "../../atoms/BaseTitle.vue";
+import BaseButton from "../../atoms/BaseButton.vue";
+import BaseLabel from "../../atoms/BaseLabel.vue";
+import BaseInput from "../../atoms/BaseInput.vue";
+import { remote } from "electron";
+import { Exec } from "../../../../database/models/Exec";
+
+@Component({
+  name: "exec-modal",
+  components: { BaseModal, BaseTitle, BaseButton, BaseLabel, BaseInput }
+})
+export default class ExecModal extends Vue {
+  // 実行名
+  private execName: string = "";
+  // ファイルパス
+  private execPath: string = "";
+
+  /**
+   * ファイル選択ダイアログ表示
+   * 選択時ファイルパスに設定
+   */
+  public clickSelectFile() {
+    const file: Array<string> | undefined = remote.dialog.showOpenDialog({
+      title: "ファイル選択",
+      defaultPath: ".",
+      properties: ["openFile"]
+    });
+    if (file) this.execPath = file[0];
+  }
+
+  /**
+   * モーダル外クリックイベント
+   */
+  @Emit("clickOutsideWindow")
+  public clickOutsideWindow() {}
+
+  /**
+   * キャンセルボタンクリックイベント
+   */
+  @Emit("clickCancel")
+  public clickCancel() {}
+
+  /**
+   * 追加ボタンクリックイベント
+   * 入力値を親コンポーネントに送る
+   */
+  @Emit("clickRegist")
+  public clickRegist() {
+    const exec: Exec = new Exec();
+    exec._name = this.execName;
+    exec._path = this.execPath;
+    return exec;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.exec-modal-content {
+  display: flex;
+  flex-direction: column;
+}
+</style>
