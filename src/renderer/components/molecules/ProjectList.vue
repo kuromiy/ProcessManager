b1ce520517e46da88fccc513090cc68810a1150b
Kuromiya <krmy3993@gmail.com>
2019-07-10 17:29:50 +0900

    ProjectList.vue 作成

---
+++ src/renderer/components/molecules/ProjectList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="project-list">
+    <BaseIcon
+      v-for="(project, index) in projects"
+      :key="index"
+      @click="clickProjectIcon(project)">
+      {{project._name}}
+    </BaseIcon>
+    <BaseIcon
+      @click="clickAddProject">
+      add
+    </BaseIcon>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+import BaseIcon from "../atoms/BaseIcon.vue";
+import { Project } from "../../../database/models/Project";
+
+@Component({
+  name: "project-list",
+  components: { BaseIcon }
+})
+export default class ProjectList extends Vue {
+  @Prop({ type: Array, required: true }) readonly projects: Array<Project>;
+
+  @Emit("clickAddProject")
+  public clickAddProject() {}
+
+  @Emit("clickProjectIcon")
+  public clickProjectIcon(project: Project) {
+    return project;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.project-list {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  flex-grow: 2;
+
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+</style>
+
