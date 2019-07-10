da6a646a1f983684aaa6701fddff61b715705d53
Kuromiya <krmy3993@gmail.com>
2019-07-10 18:53:09 +0900

    SettingPageContent 作成

---
+++ src/renderer/components/molecules/settingpage/SettingPageContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseTable
+    :headers="headers"
+    :datas="execs">
+    <template v-slot:default="slotProps">
+      <td>{{slotProps.data._id}}</td>
+      <td>{{slotProps.data._name}}</td>
+      <td>{{slotProps.data._path}}</td>
+    </template>
+  </BaseTable>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import BaseTable from "../../atoms/BaseTable.vue";
+import { Exec } from "../../../../database/models/Exec";
+
+@Component({
+  name: "setting-page-content",
+  components: { BaseTable }
+})
+export default class SettingPageContent extends Vue {
+  @Prop({ type: Array, required: true }) readonly execs: Array<Exec>;
+  private headers: Array<string> = [
+    "No.", "名前", "パス"
+  ];
+}
+</script>
