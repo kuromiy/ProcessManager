1b64ddcecdb59db5036613208f64b684106aee54
Kuromiya <krmy3993@gmail.com>
2019-07-10 18:23:03 +0900

    SideBar.vue 作成

---
+++ src/renderer/components/organisms/SideBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="side-bar">
+    <!-- ホームアイコン -->
+    <BaseIcon
+      @click="clickHomePage">
+      Home
+    </BaseIcon>
+    <!-- 登録されているプロジェクトアイコン一覧 -->
+    <ProjectList
+      :projects="projects"
+      @clickProjectIcon="clickProjectIcon"
+      @clickAddProjectIcon="clickAddProjectIcon">
+    </ProjectList>
+    <!-- 設定アイコン -->
+    <BaseIcon
+      @click="clickSettingPage">
+      Setting
+    </BaseIcon>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import BaseIcon from "../atoms/BaseIcon.vue";
+import ProjectList from "../molecules/ProjectList.vue";
+import { Project } from "../../../database/models/Project";
+import { projectModule } from "../../store/modules/ProjectModule";
+import { modalModule } from "../../store/modules/ModalModule";
+
+@Component({
+  name: "side-bar",
+  components: { BaseIcon, ProjectList }
+})
+export default class SideBar extends Vue {
+  /**
+   * ストアのProject一覧を返す。
+   */
+  public get projects() {
+    return projectModule.projects;
+  }
+
+  /**
+   * ホーム画面へ遷移
+   */
+  public clickHomePage() {
+    this.$router.push({path: "/"});
+  }
+
+  /**
+   * クリックしたプロジェクトアイコンのプロジェクト画面へ遷移
+   */
+  public clickProjectIcon(project: Project) {
+    this.$router.push({name: "project-page", params: {projectid: String(project._id)}});
+  }
+
+  /**
+   * プロジェクト追加モーダル表示
+   */
+  public clickAddProjectIcon() {
+    modalModule.openProjectModal();
+  }
+
+  /**
+   * 設定画面へ遷移
+   */
+  public clickSettingPage() {
+    this.$router.push({path: "/setting"});
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.side-bar {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  overflow: hidden;
+}
+</style>
