8100e1d800c09ec7a6ee5e52526e4219792f4bf6
Kuromiya <krmy3993@gmail.com>
2019-07-10 20:00:58 +0900

    ProjectPageHeader 作成

---
+++ src/renderer/components/molecules/projectpage/ProjectPageHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="project-page-header">
+    <div class="project-page-header__row">
+      <BaseTitle>{{projectTitle}}</BaseTitle>
+      <BaseButton @click="clickAddProcess">追加</BaseButton>
+    </div>
+    <BaseDescription :description="projectDescription" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+import BaseTitle from "../../atoms/BaseTitle.vue";
+import BaseButton from "../../atoms/BaseButton.vue";
+import BaseDescription from "../../atoms/BaseDescription.vue";
+
+@Component({
+  name: "project-page-header",
+  components: { BaseTitle, BaseButton, BaseDescription }
+})
+export default class ProjectPageHeader extends Vue {
+  @Prop(String) readonly projectTitle!: string;
+  @Prop(String) readonly projectDescription!: string;
+
+  @Emit("clickAddProcess")
+  public clickAddProcess() {}
+}
+</script>
+
+<style lang="scss" scoped>
+.project-page-header {
+  display: flex;
+  flex-direction: column;
+
+  &__row {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
